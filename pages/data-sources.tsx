8a64f37e5ece9e8f958619708533419357b07143
polymahh <otman.elkantaoui@gmail.com>
2023-02-05 12:38:00 +0100

    exporting getstaticprops

--- pages/data-sources.tsx
+++ pages/data-sources.tsx
@@ -4,10 +4,13 @@ import DataContext from "@/context/DataContext";
 import { Box, VStack } from "@chakra-ui/react";
 import { useContext, useEffect } from "react";
 
+export { getStaticProps } from "../context/DataContext";
+
 const DataSources = () => {
   const { handleDataSources, results } = useContext(DataContext);
 
   useEffect(() => {
+    console.log(results);
     handleDataSources(results);
   });
   return (
